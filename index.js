ec85f5b90609a992aec3421ff9f78c1b877f525e
anjumanara28 <anjuanny2024@gmail.com>
2025-02-13 21:57:05 +0000

    all users loaded

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sucjx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sucjx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -24,43 +22,51 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    const articleCollection = client.db("newsDB").collection("articles");
+    const publisherCollection = client.db("newsDB").collection("publisher");
+    const userCollection = client.db("newsDB").collection("users");
 
-   const articleCollection = client.db('newsDB').collection('articles')
-   const publisherCollection = client.db('newsDB').collection('publisher')
-   const userCollection = client.db('newsDB').collection('users')
-   
     //  get article post
-    app.get('/articles',async(req,res)=>{
-       const result = await articleCollection.find().sort({ views: -1 }).limit(6).toArray()
-       res.send(result)
-    })
+    app.get("/articles", async (req, res) => {
+      const result = await articleCollection
+        .find()
+        .sort({ views: -1 })
+        .limit(6)
+        .toArray();
+      res.send(result);
+    });
 
     // add articles to db
-    app.post('/articles',async (req,res)=>{
+    app.post("/articles", async (req, res) => {
       const article = req.body;
       const result = await articleCollection.insertOne(article);
-      res.send(result)
-    })
+      res.send(result);
+    });
 
-     //  get publisher 
-     app.get('/publisher',async(req,res)=>{
-      const result = await publisherCollection.find().toArray()
-      res.send(result)
-   })
+    //  get publisher
+    app.get("/publisher", async (req, res) => {
+      const result = await publisherCollection.find().toArray();
+      res.send(result);
+    });
 
-  //  user api
-  app.post('/users', async (req, res) => {
-    const user = req.body;
-    
-    const query = { email: user.email }
-    const existingUser = await userCollection.findOne(query);
+    //  user api
+    app.post("/users", async (req, res) => {
+      const user = req.body;
 
-    if (existingUser) {
-      return res.send({ message: 'user already exists', insertedId: null })
-    }
-    const result = await userCollection.insertOne(user);
-    res.send(result);
-  });
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+
+      if (existingUser) {
+        return res.send({ message: "user already exists", insertedId: null });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
 
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
@@ -69,8 +75,6 @@ async function run() {
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
-
-
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -85,5 +89,3 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`news hub is running on port ${port}`);
 });
-
-
